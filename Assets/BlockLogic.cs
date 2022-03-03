b762a7c079be9e4d8c6c071c88e29b4560c10bf9
Brown <1ilbrown@seattleschools.org>
2022-03-03 13:23:22 -0800

    First

---
+++ Assets/BlockLogic.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockLogic : MonoBehaviour
+{
+	GameLogic gameLogic;
+	public GameObject rig;
+	bool movable = true;
+    float timer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    gameLogic = FindObjectOfType<GameLogic>();
+    }
+	void RegisterBlock()
+	{
+		foreach (Transform subBlock in rig.transform)
+		{
+			 gameLogic.grid[Mathf.FloorToInt(subBlock.position.x), Mathf.FloorToInt(subBlock.position.y)] = subBlock;
+		}
+	}
+	
+	bool CheckValid()
+	{
+		foreach (Transform subBlock in rig.transform)
+		{
+			
+			if(subBlock.transform.position.x >= GameLogic.width ||
+				subBlock.transform.position.x < 0 ||
+			   subBlock.transform.position.y < 0)
+			{
+				return false;
+			}
+			if (subBlock.position.y < GameLogic.height && gameLogic.grid[Mathf.FloorToInt(subBlock.position.x),Mathf.FloorToInt(subBlock.position.y)] != null)
+			{
+				return false;
+			}
+		}
+		 return true;
+	}
+
+    // Update is called once per frame
+    void Update()
+  {
+	if (movable)
+	{
+		 //Update the timer
+        timer += 1 * Time.deltaTime;
+        //Drop
+        if(Input.GetKey(KeyCode.DownArrow) && timer > GameLogic.quickDropTime)
+        {
+            gameObject.transform.position -= new Vector3(0, 1, 0);
+	        timer = 0f;
+	        if (!CheckValid())
+	        {
+	        	movable = false;
+	        	gameObject.transform.position += new Vector3(0, 1, 0);
+	        	gameLogic.ClearLines();
+	        	gameLogic.SpawnBlock();
+	        }
+        }
+        else if(timer > GameLogic.dropTime)
+        {
+            gameObject.transform.position -= new Vector3(0, 1, 0);
+	        timer = 0f;
+	        if (!CheckValid())
+	        {
+	        	movable = false;
+	        	gameObject.transform.position += new Vector3(0, 1, 0);
+	        	RegisterBlock();
+		        gameLogic.ClearLines();
+	        	gameLogic.SpawnBlock();
+	        }
+        }
+        //Sideways
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+	        gameObject.transform.position -= new Vector3(1, 0, 0);
+	        if (!CheckValid())
+	        {
+	        	
+	        	gameObject.transform.position += new Vector3(1, 0, 0);
+	        }
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+	        gameObject.transform.position += new Vector3(1, 0, 0);
+	        if (!CheckValid())
+	        {
+	        	
+	        	gameObject.transform.position -= new Vector3(1, 0, 0);
+	        }
+        }
+		//Rotation
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rig.transform.eulerAngles -= new Vector3(0, 0 , 90);
+			if (!CheckValid())
+			{
+				rig.transform.eulerAngles += new Vector3(0, 0 , 90);
+				
+			}
+		}
+	}
+  }
+}
---
+++ Assets/GameLogic.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    public static float dropTime = 0.9f;
+	public static float quickDropTime = 0.05f;
+	public static int width = 15, height = 30;
+	public GameObject[] blocks;
+	public Transform[,] grid = new Transform[ width, height];
+    
+    void Start()
+    {
+	    SpawnBlock();
+    }
+    
+	public void ClearLines()
+	{
+		for (int y = 0; y < height; y++)
+		{
+			if(IsLineComplate(y))
+			{
+				DestroyLine(y);
+				MoveLine(y);
+			}
+		}
+		
+	}
+	
+	void MoveLine(int y)
+	{
+		for(int i = y; i < height; i++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				if(grid[x,y+1] != null)
+				{
+					grid[x,y] = grid[x,y + 1];
+					grid[x,y].gameObject.transform.position -= new Vector3(0, 1, 0);
+					grid[x,y + 1] = null;
+				}
+			}
+		}
+	}
+	void DestroyLine(int y)
+	{
+		for(int x = 0; x < width; x++)
+		{
+			Destroy(grid[x,y].gameObject);
+		}
+	}
+	bool IsLineComplate(int y)
+	{ 
+		for (int x = 0; x < width; x++)
+		{
+			if(grid[x,y]==null)
+			{
+				return false;
+			}
+		}
+		return true;
+		
+	}
+
+   
+	public void SpawnBlock()
+	{
+		float guess = Random.Range(0, 1f);
+		guess *= blocks.Length;
+		Instantiate(blocks[Mathf.FloorToInt(guess)]);
+	}
+}
